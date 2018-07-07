2b10f0c1c78d38bb03511b351dce16fcec25375d
Raseshwari <rp3737@rit.edu>
2018-07-07 17:57:06 -0400

    middleware for logging and handlebars

--- server.js
+++ server.js
@@ -1,26 +1,57 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+//registering Partials
+hbs.registerPartials(__dirname+'/views/partials');
 //using handlebars i.e., set handlebars
 app.set('view engine','hbs');
-//middleware
+
+//middleware for logging
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log + '\n', (err)=>{
+		if(err){
+			console.log('Unable to append to server log');
+		}
+	});
+	next();
+});
+
+//middlewar without next(); 
+app.use((req, res, next) => {
+	res.render('maintenance.hbs');
+})
+
+//middleware for static content like help
 app.use(express.static(__dirname+'/public'));
+
+//registering hbs helpers
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+})
 
 //Route handlers
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
+		pageBody: 'Home Page Body',
 		welcomeMessage: 'Welcome User',
-		currentYear: new Date().getFullYear()
 	});
 });
 
 app.get('/about', (req, res) => {
 	res.render('about.hbs',{
 		pageTitle: 'About Page',
-		currentYear: new Date().getFullYear()
+		pageBody: 'About Page Body',
 	});
 });
 
